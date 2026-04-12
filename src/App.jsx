35570dd06da46cb074d3163f94dadc9ade823e4e
Mehedi Hasan Mashuk <mashuk.iem.kuet@gmail.com>
2026-04-13 01:45:49 +0600

    stats section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./Components/Hero/Hero";
 import NavBar from "./Components/NavBar/NavBar";
+import Stats from "./Components/Stats/Stats";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Hero></Hero>
+      <Stats></Stats>
     </>
   );
 }
---
+++ src/Components/Stats/Stats.jsx
@@ -0,0 +1,20 @@
+const Stats = () => {
+  return (
+    <div className="bg-[#4F39F6] flex justify-between items-center px-24 py-5">
+      <div>
+        <h1 className="text-4xl font-bold text-white">50K+</h1>
+        <p className="text-white/70">Active Users</p>
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold text-white">200+</h1>
+        <p className="text-white/70">Premium Tools</p>
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold text-white">4.9</h1>
+        <p className="text-white/70">Rating</p>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
